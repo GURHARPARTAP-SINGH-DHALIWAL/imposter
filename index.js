47c338d6ab84e6f25b028e6680755d5d9597a60b
Gurharpartap Singh <gurharpartapdhaliwal@gmail.com>
2021-08-25 23:35:17 +0530

    set routes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express=require('express');
-
+const path=require('path');
 const dotenv=require('dotenv');
 const morgan=require('morgan');
 const exphbs=require('express-handlebars');
 const connectDB=require('./config/db');
+const router = require('./routes');
 
 
 dotenv.config({path:"./config/config.env"});
@@ -22,10 +23,15 @@ if(process.env.NODE_ENV==='development')
 
 // Set up HBS
 // setting the extension as .hbs  
+// actually it tells hbs to handle files with .hbs extension
+// Whwn we render a file express look for view engine and view set the layout file whihc is by default main in handlebars
 app.engine('.hbs', exphbs({defaultLayout:'main',extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+app.use(express.static('public'));
 
+// Set up routes
+app.use('/',require('./routes/index'));
 
 const port=process.env.PORT||8000;
 
